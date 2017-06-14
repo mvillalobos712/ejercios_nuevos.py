325a8a0d30273c97203f6b6155b6698f690b303e
mvillalobos712 <mvillalobos712@gmail.com>
2017-06-14 11:59:24 -0400

    Create ejerccicio4.py

---
+++ ejerccicio4.py
@@ -0,0 +1,14 @@
+#descripcion: desarrollar un programa que le pregunte al usuario por un numero y ese numero sea divisor 
+#entrada: preguntar al usuario por un numero 
+#salida: divisores del nuemro ejecutado el cual es 24
+#autor: mvillalobos712
+#fecha: 14-6-2017
+#version: 2.0
+#plataforma: python 2,7
+
+n = int(input("numero:"))
+for i in range(n):
+  if (i%2 ==0):
+     print(i)
+     
+  
