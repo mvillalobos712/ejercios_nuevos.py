4ac2d81a020733216f23d5bfb460877ef33a8c6b
mvillalobos712 <mvillalobos712@gmail.com>
2017-07-03 14:39:11 -0400

    Create ejercios_nuevos.py

---
+++ ejercios_nuevos.py
@@ -0,0 +1,13 @@
+#descripcion: este es un programa que imprime numeros menores a 5 en la siguiente lista a = [1,1,2,3,5,8,13,21,34,55,89]
+#entrada: a = [1,1,2,3,5,8,13,21,34,55,89]
+#salida: numeros menores a 5
+#autor: mvillalobos
+#fecha: 3/7/2017
+#version: 2.0 
+#plataforma: python 2.7
+
+a = [1,1,2,3,5,8,13,21,34,55,89]
+for i in a:
+     if i < 5:
+      print(i)
+      
